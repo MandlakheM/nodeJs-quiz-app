5baf8e292d7b377b0351e199e7aff71b4e46cf06
MandlakheM <142303272+MandlakheM@users.noreply.github.com>
2024-09-19 15:04:57 +0200

    each question is now timed, and the application will close down if the time limit exceeds

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const readlineImport = require("readline");
 const { questions } = require("./questions.json");
 
-// const questions = [
-//   { question: "What's the capital of South Africa?", answer: "Pretoria" },
-//   { question: "What is 2 + 2?", answer: "4" },
-//   { question: "What is the color of the sky?", answer: "blue" },
-// ];
 // console.log(questions);
 
 let currentQuestion = 0;
 let score = 0;
+const timePerQuestion = 15;
+const totalQuizDuration = 95;
+let remainingTimeForQuestion = timePerQuestion;
+let remainingTimeForQuiz = totalQuizDuration;
 
 const readline = readlineImport.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+let questionInterval;
+let quizInterval;
 
 function askQuestion() {
   if (currentQuestion >= questions.length) {
@@ -29,14 +31,29 @@ function askQuestion() {
     console.log(`${option}: ${options[option]}`);
   }
 
-  readline.question("Your answer is: ", (answer) => {
+  remainingTimeForQuestion = timePerQuestion;
+  questionInterval = setInterval(() => {
+    remainingTimeForQuestion--;
+    process.stdout.write(
+      `\rTime left for this question: ${remainingTimeForQuestion}s `
+    );
+
+    if (remainingTimeForQuestion <= 0) {
+      clearInterval(questionInterval);
+      console.log("\nTime's up for this question!");
+      currentQuestion++;
+      askQuestion();
+    }
+  }, 1000);
+
+  readline.question("\nYour answer is: ", (answer) => {
+    clearInterval(questionInterval);
+
     if (answer.toLowerCase() === questions[currentQuestion].answer) {
       console.log("Correct!");
       score++;
     } else {
-      console.log(
-        `Wrong! The correct answer is: ${questions[currentQuestion].answer}`
-      );
+      console.log("Wrong!!");
     }
     currentQuestion++;
     askQuestion();
@@ -44,11 +61,30 @@ function askQuestion() {
 }
 
 function endQuiz() {
+  clearInterval(quizInterval);
   console.log(`\nQuiz over! Your score: ${score}/${questions.length}`);
   readline.close();
 }
 
-console.log(
-  "Welcome to the quiz! Let's test your knowledge on React, Redux, and Node.js."
-);
-askQuestion();
+function startQuiz() {
+  console.log(
+    "Welcome to the quiz! Let's test your knowledge on React, Redux, and Node.js."
+  );
+
+  quizInterval = setInterval(() => {
+    remainingTimeForQuiz--;
+    // process.stdout.write(
+    //   `\rTotal time left for the quiz: ${remainingTimeForQuiz}s `
+    // );
+
+    if (remainingTimeForQuiz <= 0) {
+      clearInterval(quizInterval);
+      console.log("\nTime's up for the entire quiz!");
+      endQuiz();
+    }
+  }, 1000);
+
+  askQuestion();
+}
+
+startQuiz();
