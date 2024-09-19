fb21ebdd60d1c65fa5e4ad1e027316e9bab52cc5
MandlakheM <142303272+MandlakheM@users.noreply.github.com>
2024-09-19 09:28:47 +0200

    the user is now asked questions in a multiple choice format .i have included a json file that contains all the questions and the file is imorted to the index.js.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const readlineImport = require("readline");
+const { questions } = require("./questions.json");
 
-const questions = [
-  { question: "What's the capital of South Africa?", answer: "Pretoria" },
-  { question: "What is 2 + 2?", answer: "4" },
-  { question: "What is the color of the sky?", answer: "blue" },
-];
+// const questions = [
+//   { question: "What's the capital of South Africa?", answer: "Pretoria" },
+//   { question: "What is 2 + 2?", answer: "4" },
+//   { question: "What is the color of the sky?", answer: "blue" },
+// ];
+// console.log(questions);
 
 let currentQuestion = 0;
 let score = 0;
@@ -20,16 +22,21 @@ function askQuestion() {
     return;
   }
 
-  const { question } = questions[currentQuestion];
+  const { question, options } = questions[currentQuestion];
 
-  readline.question(`${question} `, (answer) => {
-    if (
-      answer.toLowerCase() === questions[currentQuestion].answer.toLowerCase()
-    ) {
+  console.log(`\nQuestion ${currentQuestion + 1}: ${question}`);
+  for (const option in options) {
+    console.log(`${option}: ${options[option]}`);
+  }
+
+  readline.question("Your answer is: ", (answer) => {
+    if (answer.toLowerCase() === questions[currentQuestion].answer) {
       console.log("Correct!");
       score++;
     } else {
-      console.log("Wrong!");
+      console.log(
+        `Wrong! The correct answer is: ${questions[currentQuestion].answer}`
+      );
     }
     currentQuestion++;
     askQuestion();
@@ -37,9 +44,11 @@ function askQuestion() {
 }
 
 function endQuiz() {
-  console.log(`Quiz over! Your score: ${score}/${questions.length}`);
+  console.log(`\nQuiz over! Your score: ${score}/${questions.length}`);
   readline.close();
 }
 
-console.log("Welcome to the quiz! Lets test out your JavaScript knowledge");
+console.log(
+  "Welcome to the quiz! Let's test your knowledge on React, Redux, and Node.js."
+);
 askQuestion();
